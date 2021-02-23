821c9ca13ff794b2213b0b26bce890560fa9a2b1
黄雯锦 <598581090@qq.com>
2021-02-23 17:24:31 +0800

    feat:动画闪动

--- assets/script/LogoVirus.ts
+++ assets/script/LogoVirus.ts
@@ -3,31 +3,53 @@ const { ccclass, property } = cc._decorator;
 @ccclass
 export default class LogoVirus extends cc.Component {
     @property({ type: cc.Node })
-    circl: cc.Node = null;
+    private circl: cc.Node = null; // 圆圈
     @property({ type: cc.Node })
-    virus: cc.Node = null;
+    private virus: cc.Node = null; // 病毒
     @property({ type: cc.Node })
-    tail: cc.Node = null;
+    private tail: cc.Node = null; // 拖尾
+
+    private v2: cc.Vec2;
+    private width: number;
     // onLoad () {}
 
-    start() {
-
-    }
-
     public Begin() {
-        //this.tail.active = false;
+        // this.tail.active = false;
         cc.tween(this.tail)
             .to(0.2, { opacity: 0 })
             .start();
 
-        let _out = cc.tween().to(0.5, { opacity: 0 });
-        let _in = cc.tween().to(0.5, { opacity: 255 });
+        const _out = cc.tween().to(0.5, { opacity: 0 });
+        const _in = cc.tween().to(0.5, { opacity: 255 });
         cc.tween(this.circl)
             .sequence(_out, _in)
             .repeatForever()
             .start();
+
+        this.virusAction();
     }
 
+    public init(x, y, width) {
+        this.v2 = cc.v2(x,y);
+        this.width = width
+    }
+
+    private random(lower, upper) {
+        return Math.floor(Math.random() * (upper - lower))
+    }
+
+    private virusAction(){
+        const x = this.random(0, this.width);
+        const y = this.random(0, this.width);
+
+        const v2 = cc.v2(this.v2.x+x, this.v2.y+y);
+        cc.tween(this.node)
+            .to(0.5, {x:v2.x,y:v2.y})
+            .call(function(){
+                this.virusAction();
+            }.bind(this))
+            .start();
+    }
     // update (dt) {}
 
 
